5a3a522ba7623ef76702c18b3882ce9799d48a2f
François Michaud <fmichaud@9fevrier.com>
2018-02-24 16:55:42 +0100

    Initial import

--- test/importer.test.js
+++ test/importer.test.js
@@ -15,8 +15,9 @@ const path = require('path')
 const process = require('process')
 
 const {UndefinedError} = require('@9fv.io/undefined-error')
+const {InvalidTypeError} = require('@9fv.io/invalidtype-error')
 const {Importer} = require('../lib/importer')
-const {ImportError, PluginNotFoundError} = require('../lib/import-error')
+const {PluginNotFoundError} = require('../lib/import-error')
 
 describe('Class {Importer}', () => {
   it('should be a function', () => {
@@ -24,60 +25,68 @@ describe('Class {Importer}', () => {
   })
 })
 
-describe('Create a instance of {Importer} without argument using <new> keyword', () => {
-  it('should throw an {UndefinedError}', () => {
-    (() => { new Importer() }).should.throw(UndefinedError)
+describe('Create a instance of {Importer} ', () => {
+  it('without argument should throw an {UndefinedError}', () => {
+    (() => {
+      Importer.factory()
+    }).should.throw(UndefinedError)
   })
 
-  it('should be an instance of {Importer}', () => {
-    (new Importer({name: 'test'})).should.be.an.instanceOf(Importer)
-  })
-})
-
-describe('Create a instance of {Importer} using <new> keyword', () => {
-  it('should be an object', () => {
-    (new Importer({name: 'test'})).should.be.an.Object()
+  it('with arguments ({object}) should be an instance of {Importer}', () => {
+    (() => Importer.factory({})).should.throw(InvalidTypeError)
   })
 
-  it('should be an instance of {Importer}', () => {
-    (new Importer({name: 'test'})).should.be.an.instanceOf(Importer)
+  it('with arguments ({string}) should be an instance of {Importer}', () => {
+    (Importer.factory('test')).should.be.an.instanceOf(Importer)
+  })
+
+  it('with arguments ({string},{object}) should be an instance of {Importer}', () => {
+    (Importer.factory('test', {})).should.be.an.instanceOf(Importer)
+  })
+
+  it('with arguments ({string}, {string}) should be an instance of {Importer}', () => {
+    (Importer.factory('test', 'test')).should.be.an.instanceOf(Importer)
   })
 })
 
 describe('Lookup for plugin path ', () => {
   it('into local application should be equals to "${process.cwd()/node_modules/test"', () => {
-    (new Importer({name: 'test'}).getPluginPathFromLocal()).should.be.equal(path.join(process.cwd(), 'test'))
+    (Importer.factory('test').getPluginPathFromLocal()).should.be.equal(path.join(process.cwd(), 'test'))
   })
 
   it('into "node_modules/" should be equals to "${process.cwd()/node_modules/test"', () => {
-    (new Importer({name: 'test'}).getPluginPathFromNodeModule()).should.be.equal(path.join(process.cwd(), 'node_modules', 'test'))
+    (Importer.factory('test').getPluginPathFromNodeModule()).should.be.equal(path.join(process.cwd(), 'node_modules', 'test'))
   })
 })
 
 describe('Try to found ', () => {
   it('an existing plugin path into "node_modules/" should return the absolute path of plugin', () => {
-    (new Importer({name: '@9fv.io/undefined-error'}).doesPluginPathExists()).should.be.equal(path.join(process.cwd(), 'node_modules', '@9fv.io', 'undefined-error'))
+    (Importer.factory('@9fv.io/undefined-error').doesPluginPathExists()).should.be.equal(path.join(process.cwd(), 'node_modules', '@9fv.io', 'undefined-error'))
   })
 
   it('an unexisting plugin path into "node_modules/" should throw a {PluginNotFoundError}', () => {
-    (() => new Importer({name: '@9fv.io/heurk'}).doesPluginPathExists()).should.throw(PluginNotFoundError)
+    (() => {
+      Importer.factory('@9fv.io/heurk').doesPluginPathExists()
+    }).should.throw(PluginNotFoundError)
   })
 
   it('an existing plugin path (using extension) into local should return the absolute path of plugin', () => {
-    (new Importer({name: 'lib/import-error.js'}).doesPluginPathExists()).should.be.equal(path.join(process.cwd(), 'lib/import-error.js'))
+    (Importer.factory('lib/import-error.js').doesPluginPathExists()).should.be.equal(path.join(process.cwd(), 'lib/import-error.js'))
   })
 
   it('an existing plugin path (without extension) into local should return the absolute path of plugin', () => {
-    (new Importer({name: 'lib/import-error'}).doesPluginPathExists()).should.be.equal(path.join(process.cwd(), 'lib/import-error.js'))
+    (Importer.factory('lib/import-error').doesPluginPathExists()).should.be.equal(path.join(process.cwd(), 'lib/import-error.js'))
   })
 
   /**
-  it('an existing plugin path (using directory index.js) into local should return the absolute path of plugin', () => {
+   it('an existing plugin path (using directory index.js) into local should return the absolute path of plugin', () => {
     (new Importer({name: 'lib'}).doesPluginPathExists()).should.be.equal(path.join(process.cwd(), 'lib/index.js'))
   })
    */
 
   it('an unexisting plugin path into local path should throw a {PluginNotFoundError}', () => {
-    (() => new Importer({name: './meurk'}).doesPluginPathExists()).should.throw(PluginNotFoundError)
+    (() => {
+      Importer.factory('./meurk').doesPluginPathExists()
+    }).should.throw(PluginNotFoundError)
   })
 })
